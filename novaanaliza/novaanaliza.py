2ff9316fdc45ec041a582bc55f322029d532e1da
Enes Ceman <107486241+EnesCeman@users.noreply.github.com>
2024-01-24 19:23:43 +0100

    Add files via upload
    updated version

--- novaanaliza/novaanaliza.py
+++ novaanaliza/novaanaliza.py
@@ -1,14 +1,13 @@
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
-from openai import OpenAI
-from dotenv import load_dotenv
+import openai
 import pandas as pd
 import seaborn as sns
 import streamlit as st
 from matplotlib.ticker import PercentFormatter
 
-load_dotenv()
-client = OpenAI()
 
 def truncate_text(text, max_length):
     if len(text) > max_length:
@@ -16,7 +15,7 @@ def truncate_text(text, max_length):
     return text
 
 
-def generate_recommendation(business_data):
+def generate_recommendation(business_data, api_key):
     # Generate prompt for ChatGPT based on reviews and sentiment scores
     prompt = ""
     for index, row in business_data.iterrows():
@@ -30,12 +29,12 @@ def generate_recommendation(business_data):
     prompt = truncate_text(prompt, 4096)
 
     # Generate completion using OpenAI API
-    # client.api_key = 
-    response = client.completions.create(
-        model="gpt-3.5-turbo-instruct",
+    openai.api_key = "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"
+    response = openai.Completion.create(
+        engine="gpt-3.5-turbo-instruct",
         prompt=prompt
-        + "\nWrite me similar mail, that would get the same or higher sentiment score",
-        max_tokens=280,  # Adjusted max_tokens for a longer response
+        + "\nRevwrite the parts that have a low sentiment score, and combine the rest into mail. Display also the original mail",
+        max_tokens=974,  # Adjusted max_tokens for a longer response
     )
 
     if response.choices[0].finish_reason == "stop":
@@ -64,34 +63,50 @@ def plot_histogram(business_data):
 
 
 def main():
-    st.title("Business Recommendation")
+    st.title("Mail Recommendation")
 
     # Upload CSV file
     file = st.file_uploader("Upload a CSV file", type=["csv"])
 
     if file is not None:
-        business_data = pd.read_csv(file)
+        if not file.name.endswith(".csv"):
+            st.error("Error: Please upload a CSV file.")
+            return
+        try:
+            business_data = pd.read_csv(file)
 
-        # Display uploaded data
-        st.subheader("Uploaded Business Data")
-        st.write(business_data)
+            # Display uploaded data
+            st.subheader("Uploaded Mail Data")
+            st.write(business_data)
 
-        # Check if required columns exist
-        required_columns = ["Review", "Sentiment Score"]
-        if set(required_columns).issubset(business_data.columns):
-            st.subheader("Business Recommendation")
-            recommendation = generate_recommendation(business_data)
+            # Check if required columns exist
+            required_columns = ["Review", "Sentiment Score"]
+            if set(required_columns).issubset(business_data.columns):
+                st.subheader("Mail Recommendation")
+                api_key = os.getenv("OPENAI_API_KEY")
+                recommendation = generate_recommendation(business_data, api_key)
 
-            # Display recommendation
-            st.write("Summary Recommendation:")
-            st.write(str(recommendation))  # Convert recommendation to string
-            # Plot relative frequency histogram
-            st.subheader("Sentiment Score Distribution")
-            plot_histogram(business_data)
-        else:
-            st.write(
-                "Error: The uploaded CSV file does not contain all the required columns."
-            )
+                # Display recommendation
+                st.write("Summary Recommendation:")
+                st.write(str(recommendation))  # Convert recommendation to string
+                # Display Mail
+                st.subheader("Mail Content:")
+                mail_content = "\n".join(
+                    f"{index + 1}. {row['Review']}"
+                    for index, row in business_data.iterrows()
+                )
+                st.text(mail_content)
+                # Plot relative frequency histogram
+                st.subheader("Sentiment Score Distribution")
+                plot_histogram(business_data)
+            else:
+                st.write(
+                    "Error: The uploaded CSV file does not contain all the required columns."
+                )
+        except pd.errors.EmptyDataError:
+            st.error("Error: The uploaded CSV file is empty.")
+        except Exception as e:
+            st.error(f"An error occured: {e}")
 
 
 if __name__ == "__main__":
